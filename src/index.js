421cab15b1191b59e33197b3e8f453aa2dc1477b
yauh-ask <yauheniya.askolkava@gmail.com>
2020-10-08 22:12:46 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let brackets = bracketsConfig[i].join('')
+        if (str.includes(brackets)) {
+            str = str.replace(brackets, '')
+            i = -1
+        }
+    }
+    return str ? false : true
 }
